531a657f8eff0747bdcb195f295e52bd39108d5c
Crew Spence <csspence123@gmail.com>
2020-05-13 05:27:34 -0500

    Working for one condition.  Will continue tomorrow.

--- code_wars_atm_problem.js
+++ code_wars_atm_problem.js
@@ -8,6 +8,19 @@ Try to find minimal number of notes that must be used to repay in dollars, or ou
 Good Luck!!!
 */
 
-function solve(n) {
-  // Your code here
+const solve = (n) => {
+  const denominations = [10, 20, 50, 100, 200, 500];
+  let amountLeft = n;
+  let minimal = 0;
+  for(let i = denominations.length - 1; i > -1; i--) {
+    if(amountLeft > denominations[i]) {
+      amountLeft -= denominations[i];
+      minimal++;
+      i++;
+    }
+  }
+  if(minimal > 0) {
+    return minimal;
+  }
+  return -1;
 }
